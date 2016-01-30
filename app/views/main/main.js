88354a5f19308cbb485403ede4b9ee4dd8a1f32c
James Spence <Jamesaspence20@gmail.com>
2016-01-30 11:57:13 -0600

    test animations

--- app/views/main/main.js
+++ app/views/main/main.js
@@ -1,2 +1,25 @@
 'use strict';
 
+var page;
+
+function animate(message) {
+    message.animate({
+        opacity: 0,
+        duration: 2000
+    }).then(function () {
+        message.animate({
+            opacity: 1,
+            duration:2000
+        });
+    });
+}
+
+exports.loaded = function (args) {
+    page = args.object;
+
+    var message = page.getViewById('message');
+    animate(message);
+    setInterval(function () {
+        animate(message);
+    }, 4250);
+};
